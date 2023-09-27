4ceab6ceedde616dd2bdde898ae0f698e051bf5a
sl2902 <laxmsun@hotmail.com>
2023-09-27 17:01:28 +0530

    Initial commit

---
+++ gitpod-command.sh
@@ -0,0 +1,9 @@
+ln -s /workspace/kaggle /home/gitpod/.kaggle
+
+mkdir -p /workspace/opensearch
+mkdir -p /workspace/datasets
+sudo chown -R gitpod:gitpod /workspace/opensearch
+
+
+cd docker
+docker-compose up -d
