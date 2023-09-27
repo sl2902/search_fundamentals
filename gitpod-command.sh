5f4b6edea88486212d33f496c30367e4cec6bb2e
sl2902 <laxmsun@hotmail.com>
2023-09-27 20:11:42 +0530

    Execute persmission was missing

--- gitpod-command.sh
+++
@@ -1,9 +0,0 @@
-ln -s /workspace/kaggle /home/gitpod/.kaggle
-
-mkdir -p /workspace/opensearch
-mkdir -p /workspace/datasets
-sudo chown -R gitpod:gitpod /workspace/opensearch
-
-
-cd docker
-docker-compose up -d
--- gitpod-init.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p /workspace/kaggle
-touch /workspace/kaggle/kaggle.json
-chmod 600 /workspace/kaggle/kaggle.json
-cd docker
-docker-compose pull
